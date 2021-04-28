383dd2e0959a134a38d5ec3bd559e6402016a2ea
Juris <kikosju@gmail.com>
2021-04-28 21:28:18 +0300

    Locator .svg

--- src/test/java/TretieZadanie.java
+++ src/test/java/TretieZadanie.java
@@ -1,18 +1,14 @@
 /*
 import org.openqa.selenium.By;
 
-
+// Найти локаторы
 
 public class TretieZadanie {
 
-    private final By FOR_ALL_TITLES = By.xpath(".//span[@itemprop ='headline name']");
-    private final By FOR_ALL_COMMENTS = By.xpath(".//span[@class='article-share__item--count']");
-   private final By LANG_SWITCHER = By.xpath("//a[contains(@href,'https://rus.tvnet.lv/')]");
-    private final By PRESS_AT_LOGO = By.xpath(".//img[@alt='Postimees']");
-
-
-    // private final By OPEN_LOGO_TVNET = By.xpath(".//img[@src ='//7b1236dab95abca45083322781760e97.svg']");
-    // Ne ponjal kak .SVG otkritj... Indusi ne pomogli poka chto
+   private final By FOR_ALL_TITLES = By.xpath(".//span[@itemprop ='headline name']"); // ВСЕ Названий страниц
+   private final By FOR_ALL_COMMENTS = By.xpath(".//span[@class='article-share__item--count']"); // Всех комментов
+   private final By LANG_SWITCHER = By.xpath("//a[contains(@href,'https://rus.tvnet.lv/')]"); // Переключить на РУ
+   private final By PRESS_AT_LOGO = By.xpath(".//img[@src='mo.ee/logos/4133/7b1236dab95abca45083322781760e97.svg']"); // Лого
     }
 }
 */
